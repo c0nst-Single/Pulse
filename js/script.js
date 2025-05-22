7e208f11ac9619a67a11d2623f179a958ec54286
Andrey <kayroswww@gmail.com>
2025-05-22 23:43:54 +0300

    add modale

--- js/script.js
+++ js/script.js
@@ -80,4 +80,24 @@ $(document).ready(function () {
   }
   toggleSlide(".catalog-item__ling");
   toggleSlide(".catalog-item__back");
+
+  //modale
+  $("[data-modale=consultation]").on("click", function () {
+    $(".overlay , #consultation").fadeIn("fast");
+  });
+  $(".modale__close").on("click", function () {
+    $(".overlay , #consultation, #thanks, #order").fadeOut("slow");
+  });
+  // $(".button_mini").on("click", function () {
+  //   $(".overlay , #order").fadeIn("fast");
+  // });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modale__descr").text(
+        $(".catalog-item__subtitle").eq(i).text()
+      );
+      $(".overlay , #order").fadeIn("fast");
+    });
+  });
 });
